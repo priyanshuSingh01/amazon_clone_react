5612a44550561a2d2a3418d8dd112d33df58c836
priyanshuSingh01 <priyanshusingh1606@gmail.com>
2026-08-01 04:09:28 +0530

    Update navbar banner

--- amazon_react/src/Component/navbar/navbarbanner/navbarbanner.jsx
+++ amazon_react/src/Component/navbar/navbarbanner/navbarbanner.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const navbarbanner = () => {
   return (
-    <div>
-      
+    <div className="bg-slate-600 w-auto h-9 flex place-content-evenly items-center ">
+      <div className="flex gap-7 text-white">
+        <h2 className="text-sm font-semibold">All</h2>
+        <h2 className="text-sm font-semibold">Fresh</h2>
+        <h2 className="text-sm font-semibold">Prime Video</h2>
+        <h2 className="text-sm font-semibold">Sell</h2>
+        <h2 className="text-sm font-semibold">Bestsellers</h2>
+        <h2 className="text-sm font-semibold">Today's Deals</h2>
+        <h2 className="text-sm font-semibold">Mobiles</h2>
+        <h2 className="text-sm font-semibold">New Releases</h2>
+        <h2 className="text-sm font-semibold">Prime</h2>
+        <h2 className="text-sm font-semibold">Amazon Pay</h2>
+        <h2 className="text-sm font-semibold">Electronics</h2>
+        <h2 className="text-sm font-semibold">Customer Service</h2>
+        <h2 className="text-sm font-semibold">Home &amp; Kitchen</h2>
+        <h2 className="text-sm font-semibold">Fashion</h2>
+        <h2 className="text-sm font-semibold">Computers</h2>
+        <h2 className="text-sm font-semibold">Toys &amp; Games</h2>
+      </div>
     </div>
   )
 }
